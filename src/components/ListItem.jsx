b2ae88d29d39455903663dbc61931e95c3491cae
viewsdevelop <viewsdevelop@gmail.com>
2023-05-11 14:09:43 -0500

    Add edit functionality to ListItem component

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -2,17 +2,60 @@ import React, { useState } from 'react'
 
 function ListItem({ item, onRemove }) {
   const [isVisible, setIsVisible] = useState(true)
+  const [isEditing, setIsEditing] = useState(false)
+  const [title, setTitle] = useState(item.title)
+  const [description, setDescription] = useState(item.description)
 
   const handleRemoveClick = () => {
     setIsVisible(false)
     onRemove(item.id)
   }
 
+  const handleEditClick = () => {
+    setIsEditing(true)
+  }
+
+  const handleCancelClick = () => {
+    setIsEditing(false)
+    setTitle(item.title)
+    setDescription(item.description)
+  }
+
+  const handleSaveClick = () => {
+    // TODO: update the item with the new title and description
+    setIsEditing(false)
+  }
+
   return (
     <div style={{ display: isVisible ? 'block' : 'none' }}>
-      <p>Task: {item.title}</p>
-      <p>Description: {item.description}</p>
-      <button onClick={handleRemoveClick}>Complete ✅</button>
+      {isEditing ? (
+        <form>
+          <label>
+            Title:
+            <input
+              type="text"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+            />
+          </label>
+          <label>
+            Description:
+            <textarea
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+            />
+          </label>
+          <button onClick={handleSaveClick}>Save</button>
+          <button onClick={handleCancelClick}>Cancel</button>
+        </form>
+      ) : (
+        <>
+          <p>Task: {item.title}</p>
+          <p>Description: {item.description}</p>
+          <button onClick={handleEditClick}>Edit ✏️</button>
+          <button onClick={handleRemoveClick}>Complete ✅</button>
+        </>
+      )}
     </div>
   )
 }
