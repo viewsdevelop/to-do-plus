255c22439f3bfea071b9d08d586caea71b01ae11
viewsdevelop <viewsdevelop@gmail.com>
2023-05-16 15:53:10 -0500

    Remove Divider Material UI import

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -4,7 +4,6 @@ import CardContent from '@material-ui/core/CardContent'
 import Button from '@material-ui/core/Button'
 import TextField from '@material-ui/core/TextField'
 import Typography from '@material-ui/core/Typography'
-import Divider from '@material-ui/core/Divider'
 import { makeStyles } from '@material-ui/core/styles'
 
 const useStyles = makeStyles({
