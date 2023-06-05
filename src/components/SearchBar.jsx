b5618e869851d33d2139aff7278d736c5fd15983
viewsdevelop <viewsdevelop@gmail.com>
2023-06-05 02:48:10 -0500

    Add magnifying glass icon to the search bar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,15 +1,38 @@
 import React from 'react'
 import TextField from '@material-ui/core/TextField'
+import InputAdornment from '@material-ui/core/InputAdornment'
+import SearchIcon from '@material-ui/icons/Search'
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& .MuiOutlinedInput-input': {
+      padding: theme.spacing(1),
+      paddingLeft: theme.spacing(3),
+    },
+    '& .MuiInputAdornment-root': {
+      marginLeft: theme.spacing(1),
+    },
+  },
+}))
 
 function SearchBar({ searchQuery, handleSearchQueryChange, classes }) {
+  const styles = useStyles()
+
   return (
     <TextField
-      label="Search"
+      className={`${styles.root} ${classes.root}`}
       variant="outlined"
-      className={classes.searchInput}
+      label="Search"
       value={searchQuery}
       onChange={handleSearchQueryChange}
-      fullWidth
+      InputProps={{
+        startAdornment: (
+          <InputAdornment position="start">
+            <SearchIcon />
+          </InputAdornment>
+        ),
+      }}
     />
   )
 }
