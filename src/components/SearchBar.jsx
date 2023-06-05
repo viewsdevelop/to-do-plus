382dcb78f2aba97e43b04b7294aad564d8a4e8b5
viewsdevelop <viewsdevelop@gmail.com>
2023-06-05 04:05:17 -0500

    Ensure fixed positioning of the SearchBar at the top when scrolling through list items

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -5,6 +5,18 @@ import SearchIcon from '@material-ui/icons/Search'
 import { makeStyles } from '@material-ui/core/styles'
 
 const useStyles = makeStyles((theme) => ({
+  container: {
+    display: 'flex',
+    justifyContent: 'center',
+    position: 'sticky',
+    top: '75px',
+    zIndex: 99,
+    backgroundColor: '#FAFAFA',
+    padding: theme.spacing(3, 2, 0),
+    maxWidth: '110%',
+    marginLeft: '-5%',
+    marginRight: '-5%',
+  },
   root: {
     '& .MuiOutlinedInput-input': {
       padding: theme.spacing(1),
@@ -13,6 +25,10 @@ const useStyles = makeStyles((theme) => ({
     '& .MuiInputAdornment-root': {
       marginLeft: theme.spacing(1),
     },
+    width: '100%',
+    display: 'flex',
+    justifyContent: 'flex-end',
+    boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.1)',
   },
 }))
 
@@ -20,20 +36,24 @@ function SearchBar({ searchQuery, handleSearchQueryChange, classes }) {
   const styles = useStyles()
 
   return (
-    <TextField
-      className={`${styles.root} ${classes.root}`}
-      variant="outlined"
-      label="Search"
-      value={searchQuery}
-      onChange={handleSearchQueryChange}
-      InputProps={{
-        startAdornment: (
-          <InputAdornment position="start">
-            <SearchIcon />
-          </InputAdornment>
-        ),
-      }}
-    />
+    <div className={styles.container}>
+      <TextField
+        className={`${styles.root} ${classes.root}`}
+        variant="outlined"
+        label="Search"
+        value={searchQuery}
+        onChange={handleSearchQueryChange}
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <SearchIcon />
+            </InputAdornment>
+          ),
+        }}
+      >
+        <div className={styles.label}>Search</div>
+      </TextField>
+    </div>
   )
 }
 
