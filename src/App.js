d2f0d60782a11d059d7547804e9dfd15f4f45c48
viewsdevelop <viewsdevelop@gmail.com>
2023-06-05 03:02:06 -0500

    Refactor Material UI imports

--- src/App.js
+++ src/App.js
@@ -2,12 +2,10 @@ import './App.css'
 import React, { useState, useEffect } from 'react'
 
 // Material UI
-import { makeStyles } from '@material-ui/core/styles'
+import { makeStyles, Modal, CssBaseline } from '@material-ui/core'
 import Typography from '@material-ui/core/Typography'
 import Button from '@material-ui/core/Button'
 import Fade from '@material-ui/core/Fade'
-import { Modal } from '@material-ui/core'
-import { CssBaseline } from '@material-ui/core' // Add this line to import CssBaseline
 
 // Animations
 import { FadeLoader } from 'react-spinners'
