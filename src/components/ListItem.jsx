d3596c1fa2e7b89ddaf02e483137e811a299626a
viewsdevelop <viewsdevelop@gmail.com>
2023-06-11 01:19:26 -0500

    Amend ListItemStyles import in ListItem component file

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -4,8 +4,7 @@ import CardContent from '@material-ui/core/CardContent'
 import Button from '@material-ui/core/Button'
 import TextField from '@material-ui/core/TextField'
 import Typography from '@material-ui/core/Typography'
-import { makeStyles } from '@material-ui/core/styles'
-import ListItemStyles from '../AuthenticatedAppStyles'
+import ListItemStyles from '../ListItemStyles'
 
 function ListItem({ item, onRemove, onSave }) {
   const [isVisible, setIsVisible] = useState(true)
