e0063d89094b22ae3ad26ef7e7764dbf86bb1cf6
viewsdevelop <viewsdevelop@gmail.com>
2023-06-14 19:12:08 -0500

    Reorder styles based on names

--- src/AuthenticatedAppStyles.js
+++ src/AuthenticatedAppStyles.js
@@ -1,32 +1,12 @@
 import { makeStyles } from '@material-ui/core/styles'
 
 const useStyles = makeStyles((theme) => ({
-  welcomeMessage: {
-    fontSize: '1.5em',
-    marginTop: theme.spacing(5),
-    marginBottom: theme.spacing(2),
-    [theme.breakpoints.down('sm')]: {
-      fontSize: '1.2em',
-    },
-  },
-  searchBar: {
-    marginTop: theme.spacing(2),
-    marginBottom: theme.spacing(3),
-    width: '100%',
-    [theme.breakpoints.down('sm')]: {
-      marginBottom: theme.spacing(2),
-    },
-  },
-  searchResultsWrapper: {
-    marginTop: '20px',
-    marginBottom: theme.spacing(5),
-  },
   addNewContainer: {
-    display: 'flex',
-    justifyContent: 'center',
     alignItems: 'center',
-    minHeight: '100%',
+    display: 'flex',
     flexDirection: 'column',
+    justifyContent: 'center',
+    minHeight: '100%',
     [theme.breakpoints.down('sm')]: {
       marginTop: theme.spacing(2),
     },
@@ -34,14 +14,34 @@ const useStyles = makeStyles((theme) => ({
   addNewText: {
     fontSize: '1.5rem',
     fontWeight: 400,
-    marginTop: theme.spacing(1),
     marginBottom: theme.spacing(2),
+    marginTop: theme.spacing(1),
     opacity: 0.7,
     [theme.breakpoints.down('sm')]: {
       fontSize: '1rem',
       fontWeight: 300,
-      marginTop: theme.spacing(2),
       marginBottom: theme.spacing(5),
+      marginTop: theme.spacing(2),
+    },
+  },
+  searchBar: {
+    marginBottom: theme.spacing(3),
+    marginTop: theme.spacing(2),
+    width: '100%',
+    [theme.breakpoints.down('sm')]: {
+      marginBottom: theme.spacing(2),
+    },
+  },
+  searchResultsWrapper: {
+    marginBottom: theme.spacing(5),
+    marginTop: '20px',
+  },
+  welcomeMessage: {
+    fontSize: '1.5em',
+    marginBottom: theme.spacing(2),
+    marginTop: theme.spacing(5),
+    [theme.breakpoints.down('sm')]: {
+      fontSize: '1.2em',
     },
   },
 }))
